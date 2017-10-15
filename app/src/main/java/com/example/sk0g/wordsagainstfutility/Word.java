4895bade7e3241042f9d1e546aa8d8c259b5a12f
Ani Mehta <tenderanger@gmail.com>
2017-10-15 17:37:29 +1000

    further work completed on Word

--- app/src/main/java/com/example/sk0g/wordsagainstfutility/Word.java
+++ app/src/main/java/com/example/sk0g/wordsagainstfutility/Word.java
@@ -14,8 +14,30 @@ public class Word {
         String[] parsedArray = parseLine(dictionaryLine);
         germanWord = parsedArray[0];
     }
-}
 
-private static String[] parseLine(String dictionaryLine) {
-    return;
+    private static String[] parseLine(String dictionaryLine) {
+        String[] lines = dictionaryLine.split("\\t");
+        String[] fixedLines = new String[6];
+
+    /*  Format of fixedLines:
+     *  0: German Word
+     *  1: (nullable) gender
+     *  2: English translation
+     *  3: word type
+     */
+
+        String tempWord = lines[0];
+        // Step through the first item, find the gender, discard everything after that
+        for (int i = 1; i < (tempWord.length() - 1); i++) {
+            if (tempWord.charAt(i - 1) == '{' &&
+                    tempWord.charAt(i + 1) == '}') {
+                // German word isolated, place it in fL[0]
+                fixedLines[0] = tempWord.substring(0, i - 2)
+                // Gender found, place it in fL[1]
+                fixedLines[1] = Character.toString(tempWord.charAt(i));
+            }
+        }
+
+        return fixedLines;
+    }
 }
