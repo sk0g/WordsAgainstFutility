66808e25f5bdc540186f233f0e871a6b442aa7f8
Ani Mehta <tenderanger@gmail.com>
2017-10-15 22:57:16 +1000

    Added helper methods to Word

--- app/src/main/java/com/example/sk0g/wordsagainstfutility/Word.java
+++ app/src/main/java/com/example/sk0g/wordsagainstfutility/Word.java
@@ -13,6 +13,9 @@ public class Word {
     public Word(String dictionaryLine) {
         String[] parsedArray = parseLine(dictionaryLine);
         germanWord = parsedArray[0];
+        gender = parsedArray[1];
+        englishTranslation = parsedArray[2];
+        wordType = parsedArray[3];
     }
 
     private static String[] parseLine(String dictionaryLine) {
@@ -33,11 +36,52 @@ public class Word {
                     tempWord.charAt(i + 1) == '}') {
                 // German word isolated, place it in fL[0]
                 fixedLines[0] = tempWord.substring(0, i - 2)
+
                 // Gender found, place it in fL[1]
                 fixedLines[1] = Character.toString(tempWord.charAt(i));
             }
         }
 
+        // No gender found, dump entire word
+        if (fixedLines[0] == null) { fixedLines[0] = tempWord; }
+
+        // Add translation to fL
+        fixedLines[2] = lines[1];
+
+        // Assign type only if the type is not null
+        if (detectWordType(lines[2]) != null) { fixedLines[3] = lines[2]; }
+
         return fixedLines;
+    }
+
+    private static String detectWordType(String potentialType) {
+        if (potentialType == null) { return null; }
+        switch (potentialType) {
+            case "adj":
+                return "adjective";
+            case "adv":
+                return "adverb";
+            case "verb":
+                return potentialType;
+            case "noun":
+                return potentialType;
+            default:
+                return null;
+        }
+    }
+
+    private static String detectWordGender(char potentialGender) {
+        switch (potentialGender) {
+            case 'm':
+                return "male";
+            case 'f':
+                return "female";
+            case 'n':
+                return "neutral";
+            case 'p':
+                return "plural";
+            default:
+                return "null";
+        }
     }
 }
